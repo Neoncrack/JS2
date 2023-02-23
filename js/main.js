fff5b45b98b6492728aabf1e1597d6da2b443680
SantiagoNicolasDamonte <santidamonte123@gmai.com>
2023-02-23 20:09:02 -0300

    segundaPreEntrega

--- js/main.js
+++ js/main.js
@@ -1,36 +1,25 @@
 
+const autosDisponibles = [
+    {modelo: "Volkswagen Vento", precio: 16000, año:2023, stock: 4},
+    {modelo: "Audi A4", precio: 23000, año:2022, stock: 2},
+    {modelo: "Ford Mustang", precio: 29000, año:2023, stock: 5},
+    {modelo: "Chevrolet Cruze Ltz", precio: 14000, año:2022, stock: 9},
+    {modelo: "Fiat Cronos", precio: 10000, año:2023, stock: 13},
+]
 
-class autos {
-    constructor(modelo, año, precio){
-        this.modelo= modelo;
-        this.año= año;
-        this.precio= precio;
+autosDisponibles.forEach((auto) => {
+    console.log(auto.modelo)
+})
+
+let autoElegido = (prompt("Que auto, de la lista, le interesa?"));
+
+console.log(autosDisponibles.find((auto) => auto.modelo == autoElegido));
+
+const nuevosPrecios = autosDisponibles.map((auto) =>{
+    return{
+        modelo:auto.modelo,
+        precio:auto.precio * 1.7
     }
+})
 
-    
-}
-
-const corollaViejo= new autos("Corolla", 2020, 5000000);
-const corollaNuevo= new autos("Corolla", 2023, 11000000);
-const hiluxVieja= new autos("Hilux", 2016, 6000000);
-const hiluxNueva= new autos("Hilux", 2023, 15000000);
-const camryViejo= new autos("Camry", 2005, 36000000);
-const camryNuevo= new autos("Camry", 2013, 7000000);
-const etiosViejo= new autos("Etios", 2017, 2800000);
-const etiosNuevo= new autos("Etios", 2023, 5500000);
-
-let autoEscogido=prompt("Ingrese el modelo que le interesa: ");
-
-
-if(autoEscogido="corolla"){
-    let añoAuto= prompt("Ingrese el año del modelo que le interesa para continuar: ");
-    if(añoAuto=2020){
-        alert("El precio del TOYOTA COROLLA 2020 es de " + corollaViejo.precio)
-    } else if(añoAuto=2023){
-        alert("El precio del TOYOTA COROLLA es de " + corollaNuevo.precio)
-    } else{
-        alert("No tenemos el modelo de ese año, sepa disculpar atte: GhostCars")
-    }
-
-}
-
+console.log(nuevosPrecios);
